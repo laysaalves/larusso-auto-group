f7c5e0aaa176b46eea7afd293370a63edcc9d738
Layseiras <layseirasdev@outlook.com>
2025-02-25 11:47:31 -0300

    first commit

---
+++ src/main/java/dev/layseiras/larussoautogroup/LarussoautogroupApplication.java
@@ -0,0 +1,13 @@
+package dev.layseiras.larussoautogroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarussoautogroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarussoautogroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/layseiras/larussoautogroup/LarussoautogroupApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.layseiras.larussoautogroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarussoautogroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
