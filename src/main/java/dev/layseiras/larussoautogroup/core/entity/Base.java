86cba1f8368fb33cc1459fa8d753e79afee47527
Laysa <laysa.developer@gmail.com>
2025-02-28 19:39:34 -0300

    refactor: protect abstract class constructors and remove useless atributes

--- src/main/java/dev/layseiras/larussoautogroup/core/entity/Base.java
+++ src/main/java/dev/layseiras/larussoautogroup/core/entity/Base.java
@@ -2,9 +2,7 @@ package dev.layseiras.larussoautogroup.core.entity;
 
 import jakarta.persistence.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 @MappedSuperclass
 public abstract class Base {
@@ -21,41 +19,14 @@ public abstract class Base {
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
-    @Column(name = "start_date")
-    private LocalDate startDate;
-
-    @Column(name = "end_date")
-    private LocalDate endDate;
-
-    @Column(name = "start_time")
-    private LocalTime startTime;
-
-    @Column(name = "end_time")
-    private LocalTime endTime;
-
-    public Base() {
+    protected Base() {
     }
 
-    public Base(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt, LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
+    protected Base(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
         this.id = id;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.deletedAt = deletedAt;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-
-    public Base(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
-        this.id = id;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
-    }
-
-    public Base(Long id){
-        this.id = id;
     }
 
     public Long getId() {
@@ -88,37 +59,5 @@ public abstract class Base {
 
     public void setDeletedAt(LocalDateTime deletedAt) {
         this.deletedAt = deletedAt;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalTime endTime) {
-        this.endTime = endTime;
     }
 }
