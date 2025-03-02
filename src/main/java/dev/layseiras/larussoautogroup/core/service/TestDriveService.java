fd7c9cd02f80b2bfce57c6dc736b7f0eeee71b0e
Laysa <laysa.developer@gmail.com>
2025-03-02 20:15:44 -0300

    feat(presentation): create addNewTestDrive by carId post endpoint

---
+++ src/main/java/dev/layseiras/larussoautogroup/core/service/TestDriveService.java
@@ -0,0 +1,35 @@
+package dev.layseiras.larussoautogroup.core.service;
+
+import dev.layseiras.larussoautogroup.core.entity.Car;
+import dev.layseiras.larussoautogroup.core.entity.TestDrive;
+import dev.layseiras.larussoautogroup.infra.dtos.TestDriveResponse;
+import dev.layseiras.larussoautogroup.infra.exceptions.CarNotFoundException;
+import dev.layseiras.larussoautogroup.infra.persistence.CarRepository;
+import dev.layseiras.larussoautogroup.infra.persistence.TestDriveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestDriveService {
+    @Autowired
+    private TestDriveRepository testDriveRepo;
+
+    @Autowired
+    private CarRepository carRepo;
+
+    public TestDriveResponse addNewTestDrive(Long carId) {
+        Car car = carRepo.findById(carId)
+                .orElseThrow(() -> new CarNotFoundException("Car not found"));
+        TestDrive testDrive = new TestDrive();
+        testDrive.setCarTested(car);
+        testDrive = testDriveRepo.save(testDrive);
+        return new TestDriveResponse(testDrive.getId(), "Test drive agendado!");
+    }
+
+    public List<TestDrive> getAllTestDrives() {
+        return testDriveRepo.findAll();
+    }
+}
+
---
+++ src/main/java/dev/layseiras/larussoautogroup/infra/presentation/TestDriveController.java
@@ -0,0 +1,39 @@
+package dev.layseiras.larussoautogroup.infra.presentation;
+
+import dev.layseiras.larussoautogroup.core.entity.Car;
+import dev.layseiras.larussoautogroup.core.entity.TestDrive;
+import dev.layseiras.larussoautogroup.core.service.TestDriveService;
+import dev.layseiras.larussoautogroup.infra.dtos.ErrorMessage;
+import dev.layseiras.larussoautogroup.infra.dtos.TestDriveResponse;
+import dev.layseiras.larussoautogroup.infra.exceptions.CarNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TestDriveController {
+    @Autowired
+    private TestDriveService service;
+    @Autowired
+    private TestDriveService testDriveService;
+
+    @PostMapping("/testdrives/{carId}")
+    public ResponseEntity<?> addNewTestDrive(@PathVariable Long carId) {
+        try {
+            TestDriveResponse res = service.addNewTestDrive(carId);
+            if (res != null) {
+                return ResponseEntity.ok(res);
+            }
+        } catch (CarNotFoundException e){
+            return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/testdrives")
+    public List<TestDrive> getAllTestDrives() {
+        return testDriveService.getAllTestDrives();
+    }
+}
