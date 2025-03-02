c94a40e75cdf95e0a0faa2d5fb7a6707267d49d1
Laysa <laysa.developer@gmail.com>
2025-03-02 20:13:54 -0300

    feat(dto): create error msg and testDrive response

---
+++ src/main/java/dev/layseiras/larussoautogroup/infra/dtos/ErrorMessage.java
@@ -0,0 +1,4 @@
+package dev.layseiras.larussoautogroup.infra.dtos;
+
+public record ErrorMessage(String msg) {
+}
---
+++ src/main/java/dev/layseiras/larussoautogroup/infra/dtos/TestDriveResponse.java
@@ -0,0 +1,4 @@
+package dev.layseiras.larussoautogroup.infra.dtos;
+
+public record TestDriveResponse(Long id, String destination) {
+}
